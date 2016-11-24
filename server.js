ba9722b3de0ba04cdb18c476c4504e82f2366ce5
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2016-11-24 02:11:25 +0000

    add lib

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-
+var app = require('express')();
 var express = require('express');
-var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.sendFile(__dirname + '/' + 'index.html');
 })
 
-var server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
 
-    console.log("Example app listening at http://%s:%s", host, port)
-})
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
